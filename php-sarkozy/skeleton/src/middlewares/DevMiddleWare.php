2c8f1fe4fdf04f05001e0d9add0dcd8288107d9d
theoahga <clere.theo@gmail.com>
2023-10-25 15:17:27 +0200

    dev middleware

--- php-sarkozy/skeleton/src/middlewares/DevMiddleWare.php
+++ php-sarkozy/skeleton/src/middlewares/DevMiddleWare.php
@@ -16,13 +16,65 @@ class DevMiddleWare implements MiddlewareInterface{
     private HttpRequest $request;
 
     function intercept_request(Request $req): Request{
+        if (!$req instanceof HttpRequest){
+            throw new Exception("Not Http");
+        }
+        
+        
+        
+
+        if (str_starts_with($req->get_uri(), "/dev/")){
+            $uri = substr($req->get_uri(), 4);
+            $http_req = new HttpRequest($req->get_client(), $req->get_method(), $uri, $req->get_headers(), $req->get_body());
+            $this->dev_mode = true;
+            $this->request = $http_req;
+            return $http_req;
+        }
+
         return $req;
     }
 
 
+    const DEV_INFO_LAYOUT = <<<DEV_INFO
+    <style>
+        #framework-sarkozy-info-u0hz_9{
+            display: flex;
+            position: fixed;
+            bottom: 0px;
+            left: 0px;
+            width: 100%;
+            padding: 1em;
+            background: #ffff00;
+        }
+    </style>
+    <section id="framework-sarkozy-info-u0hz_9">
+        [[content]]
+    </section>
+    DEV_INFO;
+
+    function get_info(){
+        $content = "";
+        $arr = $this->request->get_headers();
+        foreach($arr as $k=>$v){
+            $content .= "$k: $v<br/>";
+        }
+
+        return str_replace("[[content]]",$content, DevMiddleWare::DEV_INFO_LAYOUT);
+    }
 
     function intercept_response(Response $response): Response{
-        var_dump($response->get_metadata()["headers"]);
+        if (!$response instanceof HttpResponse || !$this->dev_mode){
+            return $response;
+        }
+
+        $ctype = $response->get_content_type();
+
+        if (str_starts_with($ctype, 'text/html')){
+            $body = $response->get_body();
+            $get_info = $this->get_info();
+            $response->set_body(preg_replace('/<\\/body/' ,"$get_info</body",$body, 1));
+            $response->set_content_length(strlen($response->get_body()));
+        }
 
         return $response;
     }
